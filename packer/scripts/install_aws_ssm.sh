d10089e028835f865f6cd099a795b5c0f824458b
Andy Singleton <asing@brightprocess.com>
2024-01-25 13:24:10 +0000

    Build rework
    Remove unused features
    Remove epel
    Re-add configuration scripts

--- packer/scripts/install_aws_ssm.sh
+++ packer/scripts/install_aws_ssm.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
 sudo systemctl start amazon-ssm-agent
-sudo yum install -y epel-release
