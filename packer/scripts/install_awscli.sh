6aae2804d379193d67f00305bdc7a4229bb60ed7
Andy Singleton <asing@brightprocess.com>
2024-01-25 12:35:33 +0000

    Unzip can be quiet

--- packer/scripts/install_awscli.sh
+++ packer/scripts/install_awscli.sh
@@ -4,6 +4,6 @@ echo "Installing AWS CLI"
 sudo yum install -y unzip
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+unzip -q awscliv2.zip
 rm awscliv2.zip
 sudo ./aws/install
