238b8c21098c6b122aee5a5c13b725ce9adebc5d
Andy Singleton <asing@brightprocess.com>
2024-02-02 15:18:00 +0000

    Refactor main

--- ansible/roles/dataiku-dss/files/volume-manager/src/exceptions.py
+++ ansible/roles/dataiku-dss/files/volume-manager/src/exceptions.py
@@ -6,3 +6,11 @@ class VolumeException(Exception):
         super().__init__(message)
         self.errors = errors
         logging.critical(f"{message}, {errors}")
+
+
+class HandlerException(Exception):
+    def __init__(self, message, errors):
+        super().__init__(message)
+        self.errors = errors
+        print('Handler Errors:')
+        print(errors)
--- ansible/roles/dataiku-dss/files/volume-manager/src/main.py
+++ ansible/roles/dataiku-dss/files/volume-manager/src/main.py
@@ -8,91 +8,83 @@ from snapshot import Snapshot
 from local_volume import LocalVolume
 from volume import Volume
 from config import Config
+from exceptions import HandlerException
 
+logging.basicConfig(level=os.getenv("LOG_LEVEL", logging.INFO))
 logger = logging.getLogger(__name__)
 
 
-class HandlerException(Exception):
-    def __init__(self, message, errors):
-        super().__init__(message)
-        self.errors = errors
-        print('Handler Errors:')
-        print(errors)
+def load_config():
+    """Load and return the application configuration."""
+    return Config()
 
 
-def main():
-    logging.basicConfig(level=os.getenv("LOG_LEVEL", logging.INFO))
-
-    logger.info("Starting run")
-
-    config = Config()
+def initialize_aws_resources(config):
+    """Initialize and return AWS-related resources."""
     aws_handler = Aws(config)
     instance = Instance(aws_handler)
+    return aws_handler, instance
 
-    instance_tag_set = {
-        'Environment': instance.tags['Environment'],
-        'Name': instance.tags['Name'],
-        'Application': 'Dataiku'
-    }
 
-    volume_tag_set = {
-        'Environment': instance.tags['Environment'],
-        'Name': instance.tags['Name'],
-        'Application': 'Dataiku',
-        'DataVolume': 'True'
-    }
-
-    # Prevent race condition when previous machine is slow terminating
-    instance.wait_for_instances(
-        states=['shutting-down', 'running'],
-        additional_tags=instance_tag_set)
-
+def manage_volume(instance, aws_handler, volume_tag_set, volume_mgr):
+    """Manage the volume attachment and creation."""
     # Is there a volume attachment that matches the volume spec
     if aws_handler.match_volume_attachment(instance_id=instance.get_instance_id(), tag_set=volume_tag_set):
         logger.info(f"Volume is already attached. Exiting.")
         sys.exit(0)
 
-    snapshot = Snapshot(
-        aws_handler=aws_handler,
-        tags=volume_tag_set)
+    snapshot = Snapshot(aws_handler=aws_handler, tags=volume_tag_set)
+    snapshot_id = ""
 
-    volume_mgr = Volume(
-        event_data=instance.get_instance_data(),
-        aws_handler=aws_handler
-    )
-
-    # Snapshot is the source of truth. If it doesnt exist, create a new volume
     if snapshot.exists():
-        # Wait for snapshot, if its still creating
-        logger.info(f"Got snapshot id: {snapshot.get_snapshot_data('snapshot_id')}")
-        logger.info("Waiting for snapshot to complete")
+        logger.info(f"Snapshot ID: {snapshot.get_snapshot_data('snapshot_id')}")
         snapshot.wait_for_pending()
-
-        # Remove prior volume that used the snapshot
-        if volume_mgr.get_volume(
-                search_tag_set=volume_tag_set,
-                snapshot_id=snapshot.get_snapshot_data('snapshot_id')):
-            logger.info("Removing legacy volume tied to snapshot")
+        if volume_mgr.get_volume(search_tag_set=volume_tag_set, snapshot_id=snapshot.get_snapshot_data('snapshot_id')):
             volume_mgr.delete()
+        snapshot_id = snapshot.get_snapshot_data('snapshot_id')
 
-    # Create a new volume
-    volume_mgr.create_volume(create_tag_set=volume_tag_set)
-
-    logger.info(f"Volume {volume_mgr.get_volume_data('volume_id')},"
-                f" instance {instance.get_instance_data('instance_id')},"
-                f" snapshot {snapshot.get_snapshot_data('snapshot_id')}")
-
-    logger.info("Attaching volume")
+    volume_mgr.create_volume(create_tag_set=volume_tag_set, snapshot_id=snapshot_id)
+    logger.info(f"Volume {volume_mgr.get_volume_data('volume_id')} attached to instance {instance.get_instance_data('instance_id')} with snapshot {snapshot.get_snapshot_data('snapshot_id')}")
     volume_mgr.attach(instance.get_instance_data())
 
+
+def manage_local_volume(volume_mgr, config):
+    """Initialize and mount the local volume."""
     local_volume = LocalVolume(volume_data=volume_mgr.get_volume_data(), config=config)
-
-    logger.info("Creating / resizing filesystem")
     local_volume.initialise_filesystem()
-
-    logger.info("Mounting filesystem")
     local_volume.mount(configure_fstab=True)
     local_volume.set_mount_ownership(user='dataiku', group='dataiku')
+
+
+def main():
+    try:
+        config = load_config()
+        aws_handler, instance = initialize_aws_resources(config)
+
+        instance_tag_set = {
+            'Environment': instance.tags['Environment'],
+            'Name': instance.tags['Name'],
+            'Application': 'Dataiku'
+        }
+
+        volume_tag_set = {
+            'Environment': instance.tags['Environment'],
+            'Name': instance.tags['Name'],
+            'Application': 'Dataiku',
+            'DataVolume': 'True'
+        }
+
+        # Prevent race condition when previous machine is slow terminating
+        instance.wait_for_instances(states=['shutting-down', 'running'], additional_tags=instance_tag_set)
+        volume_mgr = Volume(
+            event_data=instance.get_instance_data(),
+            aws_handler=aws_handler
+        )
+        manage_volume(instance, aws_handler, volume_tag_set, volume_mgr)
+        manage_local_volume(volume_mgr, config)
+
+    except HandlerException as e:
+        logger.error(f"HandlerException: {e.message}", exc_info=True)
 
 
 if __name__ == "__main__":
