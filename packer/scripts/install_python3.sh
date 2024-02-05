3297e01ab13d23356e16158206e2f7a268a9dee1
Andy Singleton <asing@brightprocess.com>
2024-02-05 14:11:56 +0000

    Additional pips are required on the runner

--- packer/scripts/install_python3.sh
+++ packer/scripts/install_python3.sh
@@ -3,5 +3,8 @@
 # Install Python 3.
 sudo yum -y install python3
 
-#used by some depends in Ansible
+# used by some depends in Ansible
 sudo ln -s /bin/pip3 /usr/local/bin/pip3
+
+# Install some ansible requirements
+pip3 install boto3 botocore
